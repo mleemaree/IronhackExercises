4709eef17e6af752540aa495a4e4c51ac78c0e66
Emily MaRee <lee@mleemaree.com>
2016-01-15 18:52:51 +0100

    more chess

--- week1/chess/pieces.rb
+++ week1/chess/pieces.rb
@@ -1,85 +1,95 @@
 require 'pry'
 
 class Chess_Piece
-	def initialize
+	def initialize(piece)
 		#Can only move around on Board
+		@piece = piece
+		@piece.new
 	end
 end
 
 class Pawn < Chess_Piece
-	def initialize(init_posi = [0,0])
+	def initialize(init_posi = [1,1])
 		@init_posi = init_posi
+		@new_posi = @new_posi
 		move
 	end
 
 	def move
 		puts "Input next move."
-		new_move=gets.chomp.split(',')
-		@init_posi[1] = new_move[1].to_i
+		@new_posi=gets.chomp.split(',')
+		@init_posi[1] = @new_posi[1].to_i
+		@init_posi[0] = @new_posi[0].to_i
 		puts "Your pawn is now at: #{@init_posi}"
 	end
-
-
 end
 
-
 class Rook < Chess_Piece
-	def initialize(init_posi = [5,5])
+	def initialize(init_posi = [0,0])
 		@init_posi = init_posi
+		@new_posi = @new_posi
 		move
 	end
 
 	def move
 		puts "Input next move."
-		new_move=gets.chomp.split(',')
+		new_posi=gets.chomp.split(',')
 
-		if new_move[0].to_i == @init_posi[0]
-			@init_posi[1] = new_move[1].to_i
-		elsif new_move[1].to_i==@init_posi[1]
-			@init_posi[0] = new_move[0].to_i
+		if new_posi[0].to_i == @init_posi[0]
+			@init_posi[1] = new_posi[1].to_i
+			puts "Your Rook is now at: #{@init_posi}"
+		elsif new_posi[1].to_i==@init_posi[1]
+			@init_posi[0] = new_posi[0].to_i
+			puts "Your Rook is now at: #{@init_posi}"
 		else
 			puts "Illegal Move. Input next move."
 		end
-		puts "Your rook is now at: #{@init_posi}"
 	end
 end
 
 class Bishop < Chess_Piece
 	def initialize(init_posi = [0,2])
 		@init_posi = init_posi
+		@new_posi = @new_posi
 		move
 	end
 	def move
 		puts "Input next move."
-		new_move=gets.chomp.split(',')
-		zero_move = (new_move[0].to_i - @init_posi[0]).abs
-		one_move = (new_move[1].to_i - @init_posi[1]).abs
+		new_posi=gets.chomp.split(',')
+		zero_move = (new_posi[0].to_i - @init_posi[0]).abs
+		one_move = (new_posi[1].to_i - @init_posi[1]).abs
 
 
 		if zero_move == one_move 
-			@init_posi[0] = new_move[0].to_i
-			@init_posi[1] = new_move[1].to_i
+			@init_posi[0] = new_posi[0].to_i
+			@init_posi[1] = new_posi[1].to_i
+			puts "Your Bishop is now at: #{@init_posi}"
 		else
 			puts "Illegal Move. Input next move."
 		end
-		puts "Your bishop is now at: #{@init_posi}"
 	end
 end
 
 class Knight < Chess_Piece
 	def initialize(init_posi = [0,1])
 		@init_posi = init_posi
+		@new_posi = @new_posi
 		move
 	end
 
 	def move
 		puts "Input next move."
-		new_move=gets.chomp.split(',')
-		if (@init_posi[0] - new_move[0].to_i).abs == 2 || (@init_posi[1] - new_move[1].to_i).abs == 2 && (@init_posi[0] - new_move[0].to_i).abs ==1 || (@init_posi[1] - new_move[1].to_i).abs == 1
-		@init_posi[0] = new_move[0].to_i
-		@init_posi[1] = new_move[1].to_i
-	end
-		puts "Your Knight is now at: #{@init_posi}"
+		new_posi=gets.chomp.split(',')
+		@new_posi_x = new_posi[0].to_i
+		@new_posi_y = new_posi[1].to_i
+		if (@init_posi[0] - @new_posi_x).abs == 2 || (@init_posi[1] - @new_posi_y).abs == 2 && (@init_posi[0] - @new_posi_x).abs ==1 || (@init_posi[1] - @new_posi_y).abs == 1
+			@init_posi[0] = @new_posi_x
+			@init_posi[1] = @new_posi_y
+			puts "Your Knight is now at: #{@init_posi}"
+		else
+			puts "Illegal Move. Input next move."
+			move
+		end
 	end
 	
 end
@@ -87,59 +97,61 @@ end
 class Queen < Chess_Piece
 	def initialize(init_posi = [0,5])
 		@init_posi = init_posi
+		@new_posi = @new_posi
 		move
 	end
 
 	def move
 		puts "Input next move."
-		new_move=gets.chomp.split(',')
-		zero_move = (new_move[0].to_i - @init_posi[0]).abs
-		one_move = (new_move[1].to_i - @init_posi[1]).abs
+		new_posi=gets.chomp.split(',')
+		zero_move = (new_posi[0].to_i - @init_posi[0]).abs
+		one_move = (new_posi[1].to_i - @init_posi[1]).abs
 		if zero_move == one_move
-			@init_posi[0] = new_move[0].to_i
-			@init_posi[1] = new_move[1].to_i
-		elsif new_move[0].to_i == @init_posi[0]
-			@init_posi[1] = new_move[1].to_i
-		elsif new_move[1].to_i==@init_posi[1]
-			@init_posi[0] = new_move[0].to_i
+			@init_posi[0] = new_posi[0].to_i
+			@init_posi[1] = new_posi[1].to_i
+			puts "Your Queen is now at: #{@init_posi}"
+		elsif new_posi[0].to_i == @init_posi[0]
+			@init_posi[1] = new_posi[1].to_i
+			puts "Your Queen is now at: #{@init_posi}"
+		elsif new_posi[1].to_i==@init_posi[1]
+			@init_posi[0] = new_posi[0].to_i
+			puts "Your Queen is now at: #{@init_posi}"
 		else
 			puts "Illegal Move. Input next move."
 		end
-		puts "Your Queen is now at: #{@init_posi}"
 	end
-	
 end
 
 class King < Chess_Piece
 	def initialize(init_posi = [0,4])
 		@init_posi = init_posi
+		@new_posi = @new_posi
 		move
 	end
 
 	def move
 		puts "Input next move."
-		new_move=gets.chomp.split(',')
+		new_posi=gets.chomp.split(',')
 
-		if (@init_posi[0] - new_move[0].to_i).abs <= 1 || (@init_posi[0] - new_move[0].to_i).abs <= 1 
-			zero_move = (new_move[0].to_i - @init_posi[0]).abs
-			one_move = (new_move[1].to_i - @init_posi[1]).abs
+		if (@init_posi[0] - new_posi[0].to_i).abs <= 1 || (@init_posi[0] - new_posi[0].to_i).abs <= 1 
+			zero_move = (new_posi[0].to_i - @init_posi[0]).abs
+			one_move = (new_posi[1].to_i - @init_posi[1]).abs
 			if zero_move == one_move
-				@init_posi[0] = new_move[0].to_i
-				@init_posi[1] = new_move[1].to_i
-			elsif new_move[0].to_i == @init_posi[0]
-				@init_posi[1] = new_move[1].to_i
-			elsif new_move[1].to_i==@init_posi[1]
-				@init_posi[0] = new_move[0].to_i
+				@init_posi[0] = new_posi[0].to_i
+				@init_posi[1] = new_posi[1].to_i
+				puts "Your King is now at: #{@init_posi}"
+			elsif new_posi[0].to_i == @init_posi[0]
+				@init_posi[1] = new_posi[1].to_i
+				puts "Your King is now at: #{@init_posi}"
+			elsif new_posi[1].to_i==@init_posi[1]
+				@init_posi[0] = new_posi[0].to_i
+				puts "Your King is now at: #{@init_posi}"
 			else
 				puts "Illegal Move. Input next move."
 			end
 		else
 			puts "Illegal Move. Input next move."
 		end
-		puts "Your King is now at: #{@init_posi}"
 	end
-	
 end
 
-
-Knight.new
