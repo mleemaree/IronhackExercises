46ea23096f46f3f590a6de87bd3b891d1723db95
Emily MaRee <lee@mleemaree.com>
2016-01-24 01:18:49 +0100

    masonry fix

--- week2/movie/public/js/script.js
+++ week2/movie/public/js/script.js
@@ -1,11 +1,11 @@
 jQuery(window).on('load', function(){
 
 	var $grid = $('.grid').masonry({
-	  columnWidth: 300,
+	  columnWidth: 350,
 	  gutter: 20,
 	  isAnimated: true,
 	  fitWidth:true
 	});
-
+$grid.masonry('layout');
 
 });
