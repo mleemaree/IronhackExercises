f4229e475126e1bde1c92a7f6181bdc3334f3998
Emily MaRee <lee@mleemaree.com>
2016-02-01 20:41:42 +0100

    contact app solo falta el buscador

---
+++ week4/addressbook/app/controllers/contacts_controller.rb
@@ -0,0 +1,29 @@
+class ContactsController < ApplicationController
+	def index
+		@contacts = Contact.order(name: :asc)
+	end
+
+	def create
+    	# Create new Contact from params[:contact]
+	    contact = Contact.new(
+	      :name => params[:contact][:name],
+	      :address => params[:contact][:address],
+	      :phoneNumber => params[:contact][:phone],
+	      :email => params[:contact][:email])
+	    contact.save
+	    # Render contact's attributes
+
+	    redirect_to("/")
+ 	end
+
+ 	def fav
+ 		Contact.find(params[:id]).update_attributes(:favorite =>true)
+ 		redirect_to("/show/" + params[:id])
+ 	end
+
+ 	def search
+ 		@found = Contact.where("name LIKE ?", "%#{params[:search]}%")
+ 		redirect_to("/searched/" + params[:search])
+ 	end
+
+end
